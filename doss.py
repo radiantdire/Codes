55638c70205623471c7210dd38088232f3b61fa5
root <root@localhost.localdomain>
2018-05-29 09:19:33 -0400

    Ceph

---
+++ doss.py
@@ -0,0 +1,52 @@
+import rados,rbd
+
+
+
+def GetHumanReadable(size,precision=2):
+        suffixes=['B','KB','MB','GB','TB']
+        suffixIndex = 0
+        while size > 1024 and suffixIndex < 4:
+                suffixIndex += 1 #increment the index of the suffix
+                size = size/1024.0 #apply the division
+        return "%.*f%s"%(precision,size,suffixes[suffixIndex])
+
+
+
+
+
+
+
+
+cluster = rados.Rados(conffile='/etc/ceph/ceph.conf')
+cluster.connect()
+
+
+ri = rbd.RBD()
+pool = raw_input("Enter pool\n")
+ioctx = cluster.open_ioctx(pool)
+il = ri.list(ioctx)
+
+print('\n'.join(il))
+
+inm = raw_input("Enter image\n")
+
+image = rbd.Image(ioctx, inm)
+
+si = rbd.SnapIterator(image)
+for each in si:
+	print(each)
+
+newl = [(x['name'],x['size']) for x in si]
+#print(GetHumanReadable(sum(newl[1])))
+
+for each in newl:
+	image.remove_snap(each[0])
+
+ri.remove(ioctx, inm)
+
+print("removed image")
+
+ioctx = cluster.open_ioctx(pool)
+il = ri.list(ioctx)
+
+print('\n'.join(il))
--- zzzzzzz.py
+++ zzzzzzz.py
@@ -1,58 +1,130 @@
-import subprocess
-import rados, sys
+import rados
 import rbd
+import subprocess
+from subprocess import call
 
 def GetHumanReadable(size,precision=2):
-	suffixes=['B','KB','MB','GB','TB']
-	suffixIndex = 0
-	while size > 1024 and suffixIndex < 4:
-		suffixIndex += 1 #increment the index of the suffix
-		size = size/1024.0 #apply the division
-    	return "%.*f%s"%(precision,size,suffixes[suffixIndex])
+        suffixes=['B','KB','MB','GB','TB']
+        suffixIndex = 0
+        while size > 1024 and suffixIndex < 4:
+                suffixIndex += 1 #increment the index of the suffix
+                size = size/1024.0 #apply the division
+        return "%.*f%s"%(precision,size,suffixes[suffixIndex])
+
+def get_pool(tbs):
+	ul,tl = [],[]
+	ri = rbd.RBD()
+	for pool in cluster.list_pools():
+		ioctx1 = cluster.open_ioctx(pool)
+		for image in ri.list(ioctx1):
+			if tbs in image:
+				print("Image found in pool "+str(pool)+" with name "+str(image)+"\n")
+				return ([pool,image])
+
+
+def basic(pool):
+	ri = rbd.RBD()
+        try:
+               ioctx = cluster.open_ioctx(pool)
+        except:
+                exit()
+        inm = ri.list(ioctx)
+
+        ol = [str(o.key) for o in ioctx.list_objects()]
+        ol.sort()
+	return([ioctx,inm,ol])
 
 
 
 
-def get_image_size(rbd_names):
+
+
+def get_info():
+
+	count=0
+        il,el,inlist,tl,fl,isl1,isl2,count_list,ei = [],[],[],[],[],[],[],[],[]
+        d={}
+	for image in inm:
+        	try:
+                	il.append((str(image), str(rbd.Image(ioctx, image).stat()['block_name_prefix'].split('.')[1])))
+        	except:
+                	ei.append(str(image))
+
+
+
+	for image in il:
+		inlist=[]	
+        	for obj in ol:
+                	if image[1] in obj:
+                        	count+=1
+	                        inlist.append(ioctx.stat(obj)[0])
+
+        	count_list.append(count)
+		print(image[0], GetHumanReadable(int(sum(inlist))))
+	        d[image] = (GetHumanReadable(int(sum(inlist))), count)
 	
-	for each in rbd_names:
-		try:
-			with rbd.Image(ioctx, each) as rbd_image:
-				size = GetHumanReadable(rbd_image.size())
-				print(each+'		:'+str(size))
-		
-				
-				
+	print("                    Image Name                 :     Size of Image    :     Size of Snapshots    :      No of Objects")
+	for key,value in d.iteritems():
+        	try:
+                	with rbd.Image(ioctx, key[0]) as rbd_image:
+                        	        size = GetHumanReadable(int(rbd_image.size()))
+                                	#print(rbd_image.size())
+	                                isl1.append(rbd_image.size())
+	                                isl2.append(size)
+        	                        print(key[0],'    :    ',size,'    :    ', value[0],'     :     ', value[1])
+	        except:
+        	        pass
 
 
-		except:
-			continue
+	print('Total no of objects : ', sum(count_list))
+	print('Total size of images in long int: ', sum(isl1))
+	print('Total size of images : ', GetHumanReadable(sum(isl1)))
 
-def get_count_images(l):
+def show_menu():
+	print('\nSelect\n\n1. List Pool\n2.Delete Image\n3.Exit\n')
 
-	if len(rbd_names)>0:
-	        print(each + '                  : '+str(len(rbd_names)))
 
-if __name__ == '__main__':
+
+def do_rm(a, b, c):
+	for each in ol:
+		if a in each:
+			ioctx.remove_object(each)
+		elif b in each:
+			ioctx.remove_object(each)
+	call(c, shell=True)
 	
+
+if __name__ == "__main__":
 	
 	cluster = rados.Rados(conffile='/etc/ceph/ceph.conf')
-
 	cluster.connect()
+	show_menu()
+	#pool = raw_input("Enter pool\n")
+	option = raw_input()
+	while(option !=3):
+		if option == '1':
+			pool = raw_input("Enter pool name : \n")
+			tl = basic(pool)
+			ioctx,inm,ol=tl[0],tl[1],tl[2]
+			get_info()
+		elif option == '2':
+			tbs = raw_input("Enter image to be deleted : \n")
+			wew = get_pool(tbs)
+	
+			a = str(wew[1])
+			pool = str(wew[0])
+			tl = basic(pool)
+                        ioctx,inm,ol=tl[0],tl[1],tl[2]
+			c = 'rbd remove '+pool+'/'+a
+			get_info()
+			b = str(rbd.Image(ioctx, tbs).stat()['block_name_prefix']).split('.')[1]
+			do_rm(a,b,c)	
+		elif option == '3':
+			exit()
+		else:
+			print("Wrong option\n")
+		show_menu()
+		option = raw_input()
+	#do_rm(a,b)
 
-	l = cluster.list_pools()
-
-
-	for each in l:
-        	ioctx = cluster.open_ioctx(each)
-		#print(ioctx.__sizeof__())		
-		rbd_inst = rbd.RBD()
-
-        	rbd_names = rbd_inst.list(ioctx)
-
-		#get_count_images(l)
-		if each=='images':
-			get_image_size(rbd_names)
-
-			
 

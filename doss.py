e7011349bb8fdd118b46bc0740b835d334c15398
root <root@localhost.localdomain>
2018-05-30 08:22:41 -0400

    1.1

--- doss.py
+++ doss.py
@@ -11,46 +11,61 @@ def GetHumanReadable(size,precision=2):
         return "%.*f%s"%(precision,size,suffixes[suffixIndex])
 
 
-
-
-
-
-
-
 cluster = rados.Rados(conffile='/etc/ceph/ceph.conf')
 cluster.connect()
 
 
 ri = rbd.RBD()
-pool = raw_input("Enter pool\n")
+pool = raw_input("\nEnter pool name\n\n")
 ioctx = cluster.open_ioctx(pool)
 il = ri.list(ioctx)
 
-print('\n'.join(il))
 
-inm = raw_input("Enter image\n")
 
-image = rbd.Image(ioctx, inm)
+finlist = []
 
-si = rbd.SnapIterator(image)
-for each in si:
-	print(each)
 
-newl = [(x['name'],x['size']) for x in si]
+for img in il:
+	size1 = 0
+	size = 0
+	nob = 0
+	with rbd.Image(ioctx, img) as image:
+		size1 = image.size()
+		for snap in rbd.SnapIterator(image):
+			size += snap['size']
+			nob+=1
+	finlist.append((img, size1, size, nob))
+
+print('\n')
+for each in finlist :
+	print(each[0]+'  :  '+str(GetHumanReadable(each[1]))+'  :  '+str(GetHumanReadable(each[2]))+'  :  '+str(each[3]))
+
+if len(il) == 0:
+	print("No images in the pool")
+	exit()
+else:
+	inm = raw_input("\nEnter image name\n\n")
+
+with rbd.Image(ioctx, inm) as image:
+	si = rbd.SnapIterator(image)
+
+	newl = [(x['name'],x['size']) for x in si]
 #print(GetHumanReadable(sum(newl[1])))
 
-for each in newl:
-	image.remove_snap(each[0])
-
-image.close()
+	for each in newl:
+		image.remove_snap(each[0])
 
 try:
 	ri.remove(ioctx, inm)
-	print("removed image")
+	print("\n\nRemoved the image\n")
 except (rbd.ImageBusy):
 	print("Image is busy")
 
 ioctx = cluster.open_ioctx(pool)
 il = ri.list(ioctx)
 
-print('\n'.join(il))
+if len(il)!= 0:
+	print("List of images in pool "+pool+' : \n')
+	print('\n'.join(il))
+else:
+	print("There are no images in the pool now.")

bb82cbc6423edaede8d8f05bfdceb618f40cf6fd
root <root@localhost.localdomain>
2018-05-30 09:00:34 -0400

    1.2

--- doss.py
+++ doss.py
@@ -1,5 +1,6 @@
 import rados,rbd
-
+import prettytable
+from prettytable import PrettyTable
 
 
 def GetHumanReadable(size,precision=2):
@@ -29,16 +30,24 @@ for img in il:
 	size1 = 0
 	size = 0
 	nob = 0
-	with rbd.Image(ioctx, img) as image:
-		size1 = image.size()
-		for snap in rbd.SnapIterator(image):
-			size += snap['size']
-			nob+=1
-	finlist.append((img, size1, size, nob))
+	try : 
+		with rbd.Image(ioctx, img) as image:
+			size1 = image.size()
+			for snap in rbd.SnapIterator(image):
+				size += snap['size']
+				nob+=1
+		finlist.append((img, size1, size, nob))
+	except (rbd.ImageNotFound):
+		pass
+x = PrettyTable(["Image name", "Image Size", "Snapshots Size", "No of snapshots"])
+#x.set_field_align("Image name", "l")
+#x.set_padding_width(1)
 
 print('\n')
 for each in finlist :
-	print(each[0]+'  :  '+str(GetHumanReadable(each[1]))+'  :  '+str(GetHumanReadable(each[2]))+'  :  '+str(each[3]))
+#	print(each[0]+'  :  '+str(GetHumanReadable(each[1]))+'  :  '+str(GetHumanReadable(each[2]))+'  :  '+str(each[3]))
+	x.add_row([each[0], str(GetHumanReadable(each[1])), str(GetHumanReadable(each[2])), str(each[3])])
+print(x)
 
 if len(il) == 0:
 	print("No images in the pool")
@@ -46,15 +55,20 @@ if len(il) == 0:
 else:
 	inm = raw_input("\nEnter image name\n\n")
 
-with rbd.Image(ioctx, inm) as image:
-	si = rbd.SnapIterator(image)
 
-	newl = [(x['name'],x['size']) for x in si]
-#print(GetHumanReadable(sum(newl[1])))
+try:
 
-	for each in newl:
-		image.remove_snap(each[0])
+	with rbd.Image(ioctx, inm) as image:
+		si = rbd.SnapIterator(image)
 
+		newl = [(x['name'],x['size']) for x in si]
+	#print(GetHumanReadable(sum(newl[1])))
+
+		for each in newl:
+			image.remove_snap(each[0])
+except(rbd.ImageNotFound):
+	print("Image does not exist!")
+	exit()
 try:
 	ri.remove(ioctx, inm)
 	print("\n\nRemoved the image\n")

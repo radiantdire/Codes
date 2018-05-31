b9dc736fec010233c4547ced1e69609a184d5498
root <root@localhost.localdomain>
2018-05-31 01:24:02 -0400

    1.3

--- doss.py
+++ doss.py
@@ -15,6 +15,16 @@ def GetHumanReadable(size,precision=2):
 cluster = rados.Rados(conffile='/etc/ceph/ceph.conf')
 cluster.connect()
 
+def cluster_stats(cluster):
+	ri = rbd.RBD()
+	y = PrettyTable(['Pool Name', 'Pool Size', 'No of images'])
+	print("\nList of Pools in Cluster : \n")
+	pl = cluster.list_pools()
+	for pool in pl:
+		with cluster.open_ioctx(pool) as ioctx:
+			y.add_row([pool, str(GetHumanReadable(ioctx.get_stats()['num_bytes'])), str(len(ri.list(ioctx)))])
+	print(y)
+cluster_stats(cluster)
 
 ri = rbd.RBD()
 pool = raw_input("\nEnter pool name\n\n")
@@ -22,32 +32,35 @@ ioctx = cluster.open_ioctx(pool)
 il = ri.list(ioctx)
 
 
+def pool_stats(ioctx):
+	finlist = []
 
-finlist = []
 
+	for img in il:
+		size1 = 0
+		size = 0
+		nob = 0
+		try : 
+			with rbd.Image(ioctx, img) as image:
+				size1 = image.size()
+				for snap in rbd.SnapIterator(image):
+					size += snap['size']
+					nob+=1
+			finlist.append((img, size1, size, nob))
+		except (rbd.ImageNotFound):
+			pass
+	x = PrettyTable(["Image name", "Image Size", "Snapshots Size", "No of snapshots"])
+	#x.set_field_align("Image name", "l")
+	#x.set_padding_width(1)
 
-for img in il:
-	size1 = 0
-	size = 0
-	nob = 0
-	try : 
-		with rbd.Image(ioctx, img) as image:
-			size1 = image.size()
-			for snap in rbd.SnapIterator(image):
-				size += snap['size']
-				nob+=1
-		finlist.append((img, size1, size, nob))
-	except (rbd.ImageNotFound):
-		pass
-x = PrettyTable(["Image name", "Image Size", "Snapshots Size", "No of snapshots"])
-#x.set_field_align("Image name", "l")
-#x.set_padding_width(1)
+	print('\n')
+	for each in finlist :
+	#	print(each[0]+'  :  '+str(GetHumanReadable(each[1]))+'  :  '+str(GetHumanReadable(each[2]))+'  :  '+str(each[3]))
+		x.add_row([each[0], str(GetHumanReadable(each[1])), str(GetHumanReadable(each[2])), str(each[3])])
+	print(x)
 
-print('\n')
-for each in finlist :
-#	print(each[0]+'  :  '+str(GetHumanReadable(each[1]))+'  :  '+str(GetHumanReadable(each[2]))+'  :  '+str(each[3]))
-	x.add_row([each[0], str(GetHumanReadable(each[1])), str(GetHumanReadable(each[2])), str(each[3])])
-print(x)
+pool_stats(ioctx)
+
 
 if len(il) == 0:
 	print("No images in the pool")
@@ -79,7 +92,7 @@ ioctx = cluster.open_ioctx(pool)
 il = ri.list(ioctx)
 
 if len(il)!= 0:
-	print("List of images in pool "+pool+' : \n')
-	print('\n'.join(il))
+	print("Updated pool "+pool+' : \n')
+	pool_stats(ioctx)
 else:
 	print("There are no images in the pool now.")
